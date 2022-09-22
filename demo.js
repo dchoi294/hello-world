7c5f500ba51686da2a699a2d5b1da7dd8b882736
Don Choi <dchoi294@gmail.com>
2022-09-22 22:57:37 +0000

    lab7

--- demo.js
+++ demo.js
@@ -1,19 +1,21 @@
-function welcomeMessage (guessNumb, count) {
-    while (guessNumb != 2) {
+function welcomeMessage (guessNumb) {
+    let count = 1;
+     while (guessNumb != 2) {
         guessNumb = prompt("Wrong answer! Guess again! Guess number between 1 to 10 to continue!");
-        count++;
+        count ++;
     }
     document.write("Hello, " + askUserName + "!" +" Welcome to my demo.");
-    return count;
+    alert("You guessd " + count + " times!");
 }
 
 let askUserName = prompt("What would you like to be called?");
 let guessNumb = prompt("Guess number between 1 to 10 to continue!");
-let count;
 
 if (guessNumb != 2) {
-    let guessCount = welcomeMessage (guessNumb, count);
-    alert("You guessd " + count + " times!");
+    let guessCount = welcomeMessage (guessNumb);
+} else {
+    confirm("You got it at once!");
+    document.write("Hello, " + askUserName + "!" +" Welcome to my demo.");
 }
 
 confirm("Enjoy the site!");
