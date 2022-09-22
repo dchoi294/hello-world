16e96c06f771923dfd887cdc11a4dd39c184cff7
Don Choi <dchoi294@gmail.com>
2022-09-22 05:49:38 +0000

    lab 6 progress

--- demo.js
+++ demo.js
@@ -1,11 +1,13 @@
-let message = "Hello, there!";
-
-alert("What is your name?");
-
 let askUserName = prompt("What would you like to be called?");
+let guessNumb = prompt("Guess number between 1 to 10 to continue!")
 
 if (askUserName != "") {
+    while (guessNumb != 2) {
+        if (guessNumb == 2) {
+            guessNumb = prompt("Wrong answer! Guess again! Guess number between 1 to 10 to continue!");
+        }
+    }
     document.write("Hello, " + askUserName + "!" +" Welcome to my demo.");
-} else {
-    document.write("Welcome!, this is my demo.");
 }
+
+confirm("Enjoy the site!");
