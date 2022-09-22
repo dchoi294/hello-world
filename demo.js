fa23f54c2bf8effc0912a35b0a013f86d7c8e179
Don Choi <dchoi294@gmail.com>
2022-09-22 20:49:28 +0000

    progress of lab5

--- demo.js
+++ demo.js
@@ -1,13 +1,15 @@
-let askUserName = prompt("What would you like to be called?");
-let guessNumb = prompt("Guess number between 1 to 10 to continue!")
-
-if (askUserName != "") {
+function welcomeMessage (guessNumb) {
     while (guessNumb != 2) {
-        if (guessNumb == 2) {
-            guessNumb = prompt("Wrong answer! Guess again! Guess number between 1 to 10 to continue!");
-        }
+        guessNumb = prompt("Wrong answer! Guess again! Guess number between 1 to 10 to continue!");
     }
     document.write("Hello, " + askUserName + "!" +" Welcome to my demo.");
+}
+
+let askUserName = prompt("What would you like to be called?");
+let guessNumb = prompt("Guess number between 1 to 10 to continue!");
+
+if (guessNumb != 2) {
+    welcomeMessage ();
 }
 
 confirm("Enjoy the site!");
