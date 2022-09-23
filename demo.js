83978af0b2e99d3679f13f3cef8f8646205a4fda
Don Choi <dchoi294@gmail.com>
2022-09-23 02:13:37 +0000

    lab8

--- demo.js
+++ demo.js
@@ -8,6 +8,15 @@ function welcomeMessage (guessNumb) {
     alert("You guessd " + count + " times!");
 }
 
+function makeImage() {
+    let numb = prompt("How many balls do you want at most 7");
+    if(numb >= 0 && numb <= 7){
+        for(let i = 0; i < numb; i++) {
+            document.write('<img style="width:100px;" src="ball.jpg" alt="baseball"/>');
+        }
+    }
+}
+
 let askUserName = prompt("What would you like to be called?");
 let guessNumb = prompt("Guess number between 1 to 10 to continue!");
 
@@ -18,8 +27,7 @@ if (guessNumb != 2) {
     document.write("Hello, " + askUserName + "!" +" Welcome to my demo.");
 }
 
+makeImage();
+
 confirm("Enjoy the site!");
 
-function makeImage() {
-    
-}
